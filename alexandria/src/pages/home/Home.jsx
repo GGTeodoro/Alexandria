e2163ff9074dab93e091367f5dd886fe4c9bab8e
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-12-22 18:32:22 -0300

    Bora

---
+++ alexandria/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <main className="">
+      <h1>Alexandria G Unit</h1>
+      
+    </main>
+  )
+}
+
+export default Home;
